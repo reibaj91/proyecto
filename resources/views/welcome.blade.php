1304e534840bcec13a9b6fe11dcd81f74b416aa9
reibaj91 <javilo911@gmail.com>
2018-04-27 21:49:21 +0200

    cambio en vista

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sergio maricón
                 </div>
 
                 <div class="links">
