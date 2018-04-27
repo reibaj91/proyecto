4c25d99a57b440d5b3f440a8d1cc1f5420bb3df4
SergioTF <bullstf@gmail.com>
2018-04-27 21:53:48 +0200

    puto javi como me lia....

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sergio maricón
+                    Laravel
                 </div>
 
                 <div class="links">
