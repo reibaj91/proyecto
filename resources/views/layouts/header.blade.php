fbffeeba950f0b062a83fcd0ce4998b0741477e8
reibaj91 <javilo911@gmail.com>
2018-04-29 19:36:54 +0200

    Cambio en la plantilla

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,21 +8,8 @@
                 <span class="bottom"></span>
             </div>
             <a href="index.html" class="navbar-brand">
-                <b>MIMIN</b>
+                <b>ESCUELA VIRGEN DE GUADALUPE</b>
             </a>
-
-            <ul class="nav navbar-nav search-nav">
-                <li>
-                    <div class="search">
-                        <span class="fa fa-search icon-search" style="font-size:23px;"></span>
-                        <div class="form-group form-animate-text">
-                            <input type="text" class="form-text" required>
-                            <span class="bar"></span>
-                            <label class="label-search">Type anywhere to <b>Search</b> </label>
-                        </div>
-                    </div>
-                </li>
-            </ul>
 
             <ul class="nav navbar-nav navbar-right user-nav">
                 <li class="user-name"><span>Akihiko Avaron</span></li>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,8 +4,8 @@
         <ul class="nav nav-list">
             <li><div class="left-bg"></div></li>
             <li class="time">
-                <h1 class="animated fadeInLeft">21:00</h1>
-                <p class="animated fadeInRight">Sat,October 1st 2029</p>
+                <h1 class="animated fadeInLeft"></h1>
+                <p class="animated fadeInRight"></p>
             </li>
             <li class="active ripple">
                 <a class="tree-toggle nav-header"><span class="fa-home fa"></span> Dashboard
@@ -113,3 +113,144 @@
     </div>
 </div>
 <!-- end: Left Menu -->
+
+
+<!-- start: Mobile -->
+<div id="mimin-mobile" class="reverse">
+    <div class="mimin-mobile-menu-list">
+        <div class="col-md-12 sub-mimin-mobile-menu-list animated fadeInLeft">
+            <ul class="nav nav-list">
+                <li class="active ripple">
+                    <a class="tree-toggle nav-header">
+                        <span class="fa-home fa"></span>Dashboard
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="dashboard-v1.blade.php">Dashboard v.1</a></li>
+                        <li><a href="dashboard-v2.blade.php">Dashboard v.2</a></li>
+                    </ul>
+                </li>
+                <li class="ripple">
+                    <a class="tree-toggle nav-header">
+                        <span class="fa-diamond fa"></span>Layout
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="topnav.blade.php">Top Navigation</a></li>
+                        <li><a href="boxed.blade.php">Boxed</a></li>
+                    </ul>
+                </li>
+                <li class="ripple">
+                    <a class="tree-toggle nav-header">
+                        <span class="fa-area-chart fa"></span>Charts
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="chartjs.blade.php">ChartJs</a></li>
+                        <li><a href="morris.blade.php">Morris</a></li>
+                        <li><a href="flot.blade.php">Flot</a></li>
+                        <li><a href="sparkline.blade.php">SparkLine</a></li>
+                    </ul>
+                </li>
+                <li class="ripple">
+                    <a class="tree-toggle nav-header">
+                        <span class="fa fa-pencil-square"></span>Ui Elements
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="color.blade.php">Color</a></li>
+                        <li><a href="weather.blade.php">Weather</a></li>
+                        <li><a href="typography.blade.php">Typography</a></li>
+                        <li><a href="icons.blade.php">Icons</a></li>
+                        <li><a href="buttons.blade.php">Buttons</a></li>
+                        <li><a href="media.blade.php">Media</a></li>
+                        <li><a href="panels.blade.php">Panels & Tabs</a></li>
+                        <li><a href="notifications.blade.php">Notifications & Tooltip</a></li>
+                        <li><a href="badges.blade.php">Badges & Label</a></li>
+                        <li><a href="progress.blade.php">Progress</a></li>
+                        <li><a href="sliders.blade.php">Sliders</a></li>
+                        <li><a href="timeline.blade.php">Timeline</a></li>
+                        <li><a href="modal.blade.php">Modals</a></li>
+                    </ul>
+                </li>
+                <li class="ripple">
+                    <a class="tree-toggle nav-header">
+                        <span class="fa fa-check-square-o"></span>Forms
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="formelement.blade.php">Form Element</a></li>
+                        <li><a href="#">Wizard</a></li>
+                        <li><a href="#">File Upload</a></li>
+                        <li><a href="#">Text Editor</a></li>
+                    </ul>
+                </li>
+                <li class="ripple">
+                    <a class="tree-toggle nav-header">
+                        <span class="fa fa-table"></span>Tables
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="datatables.blade.php">Data Tables</a></li>
+                        <li><a href="handsontable.blade.php">handsontable</a></li>
+                        <li><a href="tablestatic.blade.php">Static</a></li>
+                    </ul>
+                </li>
+                <li class="ripple">
+                    <a href="calendar.blade.php">
+                        <span class="fa fa-calendar-o"></span>Calendar
+                    </a>
+                </li>
+                <li class="ripple">
+                    <a class="tree-toggle nav-header">
+                        <span class="fa fa-envelope-o"></span>Mail
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="mail-box.blade.php">Inbox</a></li>
+                        <li><a href="compose-mail.blade.php">Compose Mail</a></li>
+                        <li><a href="view-mail.blade.php">View Mail</a></li>
+                    </ul>
+                </li>
+                <li class="ripple">
+                    <a class="tree-toggle nav-header">
+                        <span class="fa fa-file-code-o"></span>Pages
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="forgotpass.blade.php">Forgot Password</a></li>
+                        <li><a href="login.blade.php">SignIn</a></li>
+                        <li><a href="reg.blade.php">SignUp</a></li>
+                        <li><a href="article-v1.blade.php">Article v1</a></li>
+                        <li><a href="search-v1.blade.php">Search Result v1</a></li>
+                        <li><a href="productgrid.blade.php">Product Grid</a></li>
+                        <li><a href="profile-v1.blade.php">Profile v1</a></li>
+                        <li><a href="invoice-v1.blade.php">Invoice v1</a></li>
+                    </ul>
+                </li>
+                <li class="ripple"><a class="tree-toggle nav-header"><span class="fa "></span> MultiLevel  <span class="fa-angle-right fa right-arrow text-right"></span> </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="view-mail.blade.php">Level 1</a></li>
+                        <li><a href="view-mail.blade.php">Level 1</a></li>
+                        <li class="ripple">
+                            <a class="sub-tree-toggle nav-header">
+                                <span class="fa fa-envelope-o"></span> Level 1
+                                <span class="fa-angle-right fa right-arrow text-right"></span>
+                            </a>
+                            <ul class="nav nav-list sub-tree">
+                                <li><a href="mail-box.blade.php">Level 2</a></li>
+                                <li><a href="compose-mail.blade.php">Level 2</a></li>
+                                <li><a href="view-mail.blade.php">Level 2</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="credits.blade.php">Credits</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<button id="mimin-mobile-menu-opener" class="animated rubberBand btn btn-circle btn-danger">
+    <span class="fa fa-bars"></span>
+</button>
+<!-- end: Mobile -->
